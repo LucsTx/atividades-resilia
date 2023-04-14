f0dc6284582e4dfdcd10272c5d35e5df384fbe22
LucsTx <129103594+LucsTx@users.noreply.github.com>
2023-04-14 19:18:58 -0300

    Add files via upload

---
+++ Exercicio1Lista.py
@@ -0,0 +1,7 @@
+salarioMinimo = 1212.00
+salarioFuncionario = float(input('Digite seu salário \n'))
+calculoSalario = (salarioFuncionario / salarioMinimo)
+print('seu salario corresponde a {:.2f}'.format(calculoSalario),'salarios minimos')
+
+
+
